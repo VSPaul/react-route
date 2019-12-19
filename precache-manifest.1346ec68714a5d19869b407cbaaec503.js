be1bd22b56315e6cf89d3e5ed6676c46e8028abb
VSPaul <vascupaul_vsp@yahoo.com>
2019-12-19 10:44:32 +0200

    Updates

---
+++ precache-manifest.1346ec68714a5d19869b407cbaaec503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e788f609da588fe6d9f3a10dabb727b6",
+    "url": "/react-route/index.html"
+  },
+  {
+    "revision": "4b15493f1e9528e19649",
+    "url": "/react-route/static/css/main.9a75c4ea.chunk.css"
+  },
+  {
+    "revision": "1a5ec9a324564336ee43",
+    "url": "/react-route/static/js/2.332d1028.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-route/static/js/2.332d1028.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b15493f1e9528e19649",
+    "url": "/react-route/static/js/main.7dae197e.chunk.js"
+  },
+  {
+    "revision": "cc0347babf3208690dbe",
+    "url": "/react-route/static/js/runtime-main.1bedfda7.js"
+  }
+]);
